7d6abe50065ec731c596057729b5337c0e5084de
Adhaliel <santiagourregou.4@gmail.com>
2024-02-05 11:41:32 -0500

    finish

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -6,6 +6,7 @@ import {
     logout,
     verifyToken
 } from '../controllers/login.controller.js';
+import { authRequired } from '../middlewares/validateToken.js'
 import { validateSchema } from '../middlewares/validator.js';
 import { loginSchema, registerSchema } from '../schemas/login.schema.js';
 
@@ -16,7 +17,7 @@ router.post('/register', validateSchema(registerSchema), register)
 router.post('/login', validateSchema(loginSchema), login)
 router.post('/logout', logout)
 router.get('/verify', verifyToken)
-router.get('/list-user', listUsers)
+router.get('/list-user', authRequired, listUsers)
 
 
 export default router
